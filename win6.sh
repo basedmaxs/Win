742a480dcee9709cc5153ef2eb4b0d5e3c79682a
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-21 22:55:34 +0700

    Create win6.sh

---
+++ win6.sh
@@ -0,0 +1,6 @@
+# WINK
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
+tar xf lolMiner_v1.24a_Lin64.tar.gz
+cd 1.24a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user WIN:TZ8cdLNKvxXRE1YakyDjsTmMkuBSdXwoco.IW6
